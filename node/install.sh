aaeb63d7ac8dcdd50f5ddd5ba39706c61c008c7a
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-28 16:00:05 +0800

    update

--- node/install.sh
+++ node/install.sh
@@ -4,6 +4,8 @@ wget https://neo4j.com/artifact.php?name=neo4j-community-4.4.3-unix.tar.gz -O ne
 tar xf neo4j-community-4.4.3-unix.tar.gz
 apk add openjdk11 --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
 java -version
-ls
-pwd
-./neo4j-community-4.4.3/bin/neo4j console
+
+
+sed -i '/dbms.default_listen_address=0.0.0.0/s/^#//g' ./neo4j-community-4.4.3/conf/neo4j.conf 
+
+#./neo4j-community-4.4.3/bin/neo4j console
