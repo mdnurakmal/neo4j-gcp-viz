160cfea75ac0cbb5daa3fe5e6cec9a9bb90f33d1
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-28 15:35:25 +0800

    update

--- node/install.sh
+++ node/install.sh
@@ -3,8 +3,9 @@
 wget https://neo4j.com/artifact.php?name=neo4j-community-4.4.3-unix.tar.gz
 tar xf
 apt update -y
-java -version
 
 apt install default-jre -y
 apt install openjdk-11-jre-headless -y
 apt install openjdk-8-jre-headless -y
+
+java -version
