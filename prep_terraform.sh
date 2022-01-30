bd7d7fabc8cddbc172fb7208f3935a3781ba21ee
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-30 17:52:37 +0800

    update

--- prep_terraform.sh
+++ prep_terraform.sh
@@ -8,14 +8,14 @@ terraform state show 'google_storage_bucket.tf-bucket'
 return_value=$?
 if [[ $return_value == 0 ]] 
 then
-    echo "Bucket exist"
+    echo "Bucket managed"
 else
-    echo "Creating new bucket..."
-    gsutil mb gs://${1}-neo4j-viz
+    echo "Bucket not managed"
+    #gsutil mb gs://${1}-neo4j-viz
 fi
 
-"cd terraform && terraform import google_storage_bucket.tf-bucket ${GOOGLE_CLOUD_PROJECT}-neo4j-viz",
-"cd terraform && terraform state pull",
-"cd terraform && terraform apply -auto-approve -var region=$GOOGLE_CLOUD_REGION"
+# "cd terraform && terraform import google_storage_bucket.tf-bucket ${GOOGLE_CLOUD_PROJECT}-neo4j-viz",
+# "cd terraform && terraform state pull",
+# "cd terraform && terraform apply -auto-approve -var region=$GOOGLE_CLOUD_REGION"
 
-cd..
+# cd..
