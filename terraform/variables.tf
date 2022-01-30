d88358f8aa17fce52f073cde3004e652ff4f27c6
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-30 17:21:22 +0800

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,17 +4,3 @@ variable "gcp_region" {
   description = "GCP region"
    default = "asia-east1"
 }
-
-# GCP authentication file
-variable "gcp_auth_file" {
-  type        = string
-  description = "GCP authentication file"
-}
-variable "bucket-name" {
-  type        = string
-  description = "The name of the Google Storage Bucket to create"
-}
-variable "storage-class" {
-  type        = string
-  description = "The storage class of the Storage Bucket to create"
-}
