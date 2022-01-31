158791c50f8ab983dde894873eb3292ca69320cc
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-31 13:44:14 +0800

    Update neo4j.tf

--- terraform/neo4j.tf
+++ terraform/neo4j.tf
@@ -8,11 +8,11 @@ resource "google_cloud_run_service" "default" {
         image = "gcr.io/${local.project}/neo4j"
         ports {
             name = "neo4j-7474"
-            container_port = 7474
+            container_port = "7474"
         }
         ports {
             name = "neo4j-7687"
-            container_port = 7687
+            container_port = "7687"
         }
       }
     }
