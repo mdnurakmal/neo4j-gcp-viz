1b17ec5e2e48328e0fc01cd45dd77d1b60e53b90
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-31 12:09:40 +0800

    update

--- terraform/build.tf
+++ terraform/build.tf
@@ -2,18 +2,8 @@ resource "null_resource" "cloudbuild" {
 
  provisioner "local-exec" {
 
-    command = "cd $HOME/neo4j-gcp-viz && gcloud builds submit -q"
+    command = "cd $HOME/neo4j-gcp-viz && gcloud builds submit --config cloudbuild.yaml $HOME/neo4j-gcp-viz/node"
   }
 
    depends_on = [google_project_service.cloudbuild]
-}
-
-resource "null_resource" "docker_pull" {
-
- provisioner "local-exec" {
-
-    command = "docker pull gcr.io/${data.google_project.project.project_id}/neo4j-gcp-viz"
-  }
-
-   depends_on = [null_resource.cloudbuild]
 }
