014e6628816614e36afb715b214fa279bc246a91
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-31 19:32:02 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -4,5 +4,7 @@
 sed -i 's/<PLACEHOLDER_BUCKET>/'$1'-neo4j-viz/g' ./terraform/main.tf
 sed -i 's/<PLACEHOLDER_PROJECTID>/'$1'/g' ./terraform/main.tf
 
+cd terraform 
 terraform init
 terraform destroy
+cd ..
