e7e88d59525c0b8873c46318c1cd8886a0c97146
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-31 13:16:21 +0800

    update

--- prep_terraform.sh
+++ prep_terraform.sh
@@ -25,6 +25,6 @@ terraform apply -auto-approve -var region=$2
 
 cd ..
 
-gcloud builds submit --config cloudbuild.yaml .
+#gcloud builds submit --config cloudbuild.yaml .
 
-docker pull gcr.io/$1/neo4j-gcp-viz
+#docker pull gcr.io/$1/neo4j-gcp-viz
