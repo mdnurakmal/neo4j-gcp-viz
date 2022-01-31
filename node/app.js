02603dd3ab4bc4e5f3a65aec4ea69d15b60d91f8
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-31 17:57:36 +0800

    update

--- node/app.js
+++ node/app.js
@@ -7,11 +7,7 @@ const port = 8080
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    var mascots = [
-        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-        { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-      ];
+
       var tagline = "No programming concept is complete without a cute animal mascot.";
     
       res.render('pages/index', {
