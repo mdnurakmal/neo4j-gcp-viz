78a1f441edcb9a573f7a1ce59d76e7cddcb738c2
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-31 14:13:01 +0800

    Update neo4j.tf

--- terraform/neo4j.tf
+++ terraform/neo4j.tf
@@ -22,7 +22,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "n1-micro"
+    machine_type = "f1-micro"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
