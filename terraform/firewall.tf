d9234b9338b64ed62d226249efbbd8fe8f1351c8
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-03 13:41:41 +0800

    Create firewall.tf

---
+++ terraform/firewall.tf
@@ -0,0 +1,39 @@
+resource "google_compute_firewall" "neo4j-ingress" {
+  name    = "neo4j-ingress"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "neo4j"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["7474", "7687", "7473"]
+  }
+
+  direction = INGRESS
+
+ depends_on[google_compute_network.default]
+}
+
+resource "google_compute_firewall" "neo4j-egress" {
+  name    = "neo4j-egress"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "neo4j"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["7474", "7687", "7473"]
+  }
+
+  direction = EGRESS
+
+    depends_on[google_compute_network.default]
+}
+
+resource "google_compute_network" "default" {
+  name = "default"
+}
